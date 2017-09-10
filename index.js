3d4288e4167e3324cd0cad48a2e42bd1e56b8f52
Kayla Hansen <shiverr@gmail.com>
2017-09-10 02:07:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,10 +6,15 @@ function theBeatlesPlay (musicians, instruments) {
   return beat;
 }
 
-function johnLennonFacts(factsAboutLennon) {
-  var facts = [];
-  while (facts.length <= 4) {
-    facts.push(factsAboutLennon[i] + '!!!')
+function johnLennonFacts() {
+  var facts = ["He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"];
+  var newFacts = [];
+  var i = 0;
+  while (facts.length > i) {
+    newFacts.push(facts[i] + '!!!');
   }
-  return facts;
+  return newFacts;
 }
